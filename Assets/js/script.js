e18f57c29df2508796aef5d88a76f84e2a3b83fd
Mike B <115735557+mbartnett@users.noreply.github.com>
2022-11-28 17:53:20 -0600

    starting on the js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,6 +2,13 @@
 
 const apiKey = '7e04a867561611c9d92b2e344ce5ac39';
 
+const currentDate = document.getElementById('current-date');
+const currentCity = document.getElementById('current-city');
+const currentEmoji = document.getElementById('current-emoji');
+const currentTemp = document.getElementById('current-temp');
+const currentWind = document.getElementById('current-wind');
+const currentHumidity = document.getElementById('current-humidity');
+
 // Object captures all location data
 
 let locationData = {
