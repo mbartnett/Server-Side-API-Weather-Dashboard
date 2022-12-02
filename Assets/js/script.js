df0aa0989995edbb85e484d7da232a2702858843
Mike B <115735557+mbartnett@users.noreply.github.com>
2022-12-02 13:35:04 -0600

    got five-day forecast populating on the page

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -21,7 +21,8 @@ function displayWeather(cityName) {
         .then(function (currentWeather) {
             console.log(currentWeather)
             let iconURL = `https://openweathermap.org/img/w/${currentWeather.weather[0].icon}.png`;
-            currentSection.innerHTML = `<h2><span id="current-city">${currentWeather.name}</span>, <span id="current-date">${(currentWeather.dt)}</span> <img style="display:inline-block;" src="${iconURL}" /></h2>
+            currentSection.innerHTML = 
+            `<h2><span id="current-city">${currentWeather.name}</span>, <span id="current-date">${(currentWeather.dt)}</span> <img style="display:inline-block;" src="${iconURL}" /></h2>
             <h4>Temp: <span id="current-temp">${currentWeather.main.temp}</span></h4>
             <h4>Wind: <span id="current-wind">${currentWeather.wind.speed} mph</span></h4>
             <h4>Humidity: <span id="current-humidity">${currentWeather.main.humidity}</span></h4>`
@@ -33,42 +34,47 @@ function displayWeather(cityName) {
                 .then(function (fiveDayWeather) {
                     for (i = 2; i < fiveDayWeather.list.length; i = i + 8) {
                         console.log(fiveDayWeather.list[i])
+                        let five0IconURL = `https://openweathermap.org/img/w/${fiveDayWeather.list[0].weather[0].icon}.png`;
+                        let five1IconURL = `https://openweathermap.org/img/w/${fiveDayWeather.list[0].weather[0].icon}.png`;
+                        let five2IconURL = `https://openweathermap.org/img/w/${fiveDayWeather.list[0].weather[0].icon}.png`;
+                        let five3IconURL = `https://openweathermap.org/img/w/${fiveDayWeather.list[0].weather[0].icon}.png`;
+                        let five4IconURL = `https://openweathermap.org/img/w/${fiveDayWeather.list[0].weather[0].icon}.png`;
                         fiveDaySection.innerHTML = 
                     `<section class="five-day-child-container">
-                        <h3>${fiveDayWeather.dt}</h3>
-                        <p>EMOJI</p>
-                        <p>Temp: ${fiveDayWeather[0].main.temp}</p>
-                        <p>Wind:</p>
-                        <p>Humidity:</p>            
+                        <h3>${fiveDayWeather.list[0].dt}</h3>
+                        <p><img style="display:inline-block;" src="${five0IconURL}" /></p>
+                        <p>Temp: ${fiveDayWeather.list[0].main.temp}</p>
+                        <p>Wind: ${fiveDayWeather.list[0].wind.speed} mph</p>
+                        <p>Humidity: ${fiveDayWeather.list[0].main.humidity}</p>            
                         </section>
                     <section class="five-day-child-container">
-                        <h3>DATE</h3>
-                        <p>EMOJI</p>
-                        <p>Temp:</p>
-                        <p>Wind:</p>
-                        <p>Humidity:</p>                   
-                    </section>
-                        <section class="five-day-child-container">
-                        <h3>DATE</h3>
-                        <p>EMOJI</p>
-                        <p>Temp:</p>
-                        <p>Wind:</p>
-                        <p>Humidity:</p>                   
+                        <h3>${fiveDayWeather.list[1].dt}</h3>
+                        <p><img style="display:inline-block;" src="${five1IconURL}" /></p>
+                        <p>Temp: ${fiveDayWeather.list[1].main.temp}</p>
+                        <p>Wind: ${fiveDayWeather.list[1].wind.speed} mph</p>
+                        <p>Humidity: ${fiveDayWeather.list[1].main.humidity}</p>                  
                     </section>
                     <section class="five-day-child-container">
-                        <h3>DATE</h3>
-                        <p>EMOJI</p>
-                        <p>Temp:</p>
-                        <p>Wind:</p>
-                        <p>Humidity:</p>                   
+                        <h3>${fiveDayWeather.list[2].dt}</h3>
+                        <p><img style="display:inline-block;" src="${five2IconURL}" /></p>
+                        <p>Temp: ${fiveDayWeather.list[2].main.temp}</p>
+                        <p>Wind: ${fiveDayWeather.list[2].wind.speed} mph</p>
+                        <p>Humidity: ${fiveDayWeather.list[2].main.humidity}</p>                  
                     </section>
-                <section class="five-day-child-container">
-                        <h3>DATE</h3>
-                        <p>EMOJI</p>
-                        <p>Temp:</p>
-                        <p>Wind:</p>
-                        <p>Humidity:</p>                   
-                </section>`
+                    <section class="five-day-child-container">
+                        <h3>${fiveDayWeather.list[3].dt}</h3>
+                        <p><img style="display:inline-block;" src="${five3IconURL}" /></p>
+                        <p>Temp: ${fiveDayWeather.list[3].main.temp}</p>
+                        <p>Wind: ${fiveDayWeather.list[3].wind.speed} mph</p>
+                        <p>Humidity: ${fiveDayWeather.list[3].main.humidity}</p>                 
+                    </section>
+                    <section class="five-day-child-container">
+                        <h3>${fiveDayWeather.list[4].dt}</h3>
+                        <p><img style="display:inline-block;" src="${five4IconURL}" /></p>
+                        <p>Temp: ${fiveDayWeather.list[4].main.temp}</p>
+                        <p>Wind: ${fiveDayWeather.list[4].wind.speed} mph</p>
+                        <p>Humidity: ${fiveDayWeather.list[4].main.humidity}</p>                    
+                    </section>`
                     }
                 })
         })
