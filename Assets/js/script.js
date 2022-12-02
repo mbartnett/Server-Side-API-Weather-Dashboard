f45824c1ba4aec78a501478a8a1f954435e242e2
Mike B <115735557+mbartnett@users.noreply.github.com>
2022-12-02 00:59:53 -0600

    trying to fix current weather module

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,7 +4,6 @@ const apiKey = '7e04a867561611c9d92b2e344ce5ac39';
 const submitBtn = document.getElementById('submit')
 let currentSection = document.getElementById('current-section');
 const searchText = document.getElementById('search-text');
-
 submitBtn.addEventListener('click', getWeather)
 
 function getWeather() {
@@ -13,7 +12,6 @@ function getWeather() {
 }
 
 function displayWeather(cityName) {
-    let dateJS = dayjs().format('M/D/YYY')
     const apiURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
     fetch(apiURL)
         .then(function (response) {
@@ -21,21 +19,21 @@ function displayWeather(cityName) {
         })
         .then(function (currentWeather) {
             console.log(currentWeather)
-            const iconURL = `http://api.openweathermap.org/img/w/${currentWeather.weather[0].icon}`
-            currentSection.innerHTML=`<h2><span id="current-city">${currentWeather.name}</span>, <span id="current-date">${dayjs(currentWeather.dt_txt).unix()}</span> <span id="current-emoji">${`<"iconURL">`}.png</span></h2>
-            <h4>Temp: <span id="current-temp"></span></h4>
-            <h4>Wind: <span id="current-wind"></span></h4>
-            <h4>Humidity: <span id="current-humidity"></span></h4>`
+            let iconURL = `https://openweathermap.org/img/w/${currentWeather.weather[0].icon}.png`;
+            currentSection.innerHTML = `<h2><span id="current-city">${currentWeather.name}</span>, <span id="current-date">${(currentWeather.dt)}</span> <img style="display:inline-block;" src="${iconURL}" /></h2>
+            <h4>Temp: <span id="current-temp">${currentWeather.main.temp}</span></h4>
+            <h4>Wind: <span id="current-wind">${currentWeather.wind.speed} mph</span></h4>
+            <h4>Humidity: <span id="current-humidity">${currentWeather.main.humidity}</span></h4>`
             const fiveDayURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${currentWeather.coord.lat}&lon=${currentWeather.coord.lon}&appid=${apiKey}&units=imperial`
-            fetch(fiveDayURL)      
-            .then(function (response){
-                return response.json()
-            })
-            .then(function (fiveDayWeather){
-                for (i = 2; i < fiveDayWeather.list.length; i = i+8){
-                    console.log(fiveDayWeather.list[i])
-                }
-            })
+            fetch(fiveDayURL)
+                .then(function (response) {
+                    return response.json()
+                })
+                .then(function (fiveDayWeather) {
+                    for (i = 2; i < fiveDayWeather.list.length; i = i + 8) {
+                        console.log(fiveDayWeather.list[i])
+                    }
+                })
         })
 }
 
