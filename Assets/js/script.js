e27ebd0b4d180412d4c09129eae84599993807cb
Mike B <115735557+mbartnett@users.noreply.github.com>
2022-12-01 18:26:43 -0600

    adding changes from office hours

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,40 +1,21 @@
 // API stuff
 
 const apiKey = '7e04a867561611c9d92b2e344ce5ac39';
-
-var submitBtn = document.getElementById('submit')
-
-var currentSection = document.getElementById('current-section');
-
-// Elements by ID
-
+const submitBtn = document.getElementById('submit')
+let currentSection = document.getElementById('current-section');
 const searchText = document.getElementById('search-text');
-
-// const currentDate = document.getElementById('current-date');
-// const currentCity = document.getElementById('current-city');
-// const currentEmoji = document.getElementById('current-emoji');
-// const currentTemp = document.getElementById('current-temp');
-// const currentWind = document.getElementById('current-wind');
-// const currentHumidity = document.getElementById('current-humidity');
-
-// Object captures all location data
-
-let locationData = {
-    city: '',
-    state: '',
-    country: '',
-    coordinates: ''
-};
 
 submitBtn.addEventListener('click', getWeather)
 
 function getWeather() {
     var cityName = searchText.value
-    alert(cityName)
     displayWeather(cityName);
 }
 
+
+
 function displayWeather(cityName) {
+    let dateJS = dayjs().format('M/D/YYY')
     const apiURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
     fetch(apiURL)
         .then(function (response) {
@@ -42,7 +23,8 @@ function displayWeather(cityName) {
         })
         .then(function (currentWeather) {
             console.log(currentWeather)
-            currentSection.innerHTML=` <h2><span id="current-city">${currentWeather.name}</span>, <span id="current-date">${dayjs(currentWeather.dt).unix()}</span> <span id="current-emoji">EMOJI</span></h2>
+            const iconURL = `http://api.openweathermap.org/img/w/${currentWeather.weather[0].icon}`
+            currentSection.innerHTML=`<h2><span id="current-city">${currentWeather.name}</span>, <span id="current-date">${dayjs(currentWeather.dt_txt).unix()}</span> <span id="current-emoji">${`<"iconURL">`}.png</span></h2>
             <h4>Temp: <span id="current-temp"></span></h4>
             <h4>Wind: <span id="current-wind"></span></h4>
             <h4>Humidity: <span id="current-humidity"></span></h4>`
