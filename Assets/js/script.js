d1f407ec320c696d1270d8754a6388bcf19c202b
Mike B <115735557+mbartnett@users.noreply.github.com>
2022-12-06 18:48:06 -0600

    working on emojis

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -14,7 +14,6 @@ function getWeather() {
 
 function displayWeather(cityName) {
     const apiURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
-    let emoji = "";
     fetch(apiURL)
         .then(function (response) {
             return response.json()
@@ -23,7 +22,7 @@ function displayWeather(cityName) {
             console.log(currentWeather)
             let iconURL = `https://openweathermap.org/img/w/${currentWeather.weather[0].icon}.png`;
             currentSection.innerHTML =
-                `<h2><span id="current-city">${currentWeather.name}</span>, <span id="current-date">${dayjs.unix(currentWeather.dt).format('MMMM D, YYYY')}</span> ${emoji} <img style="display:inline-block;" src="${iconURL}" /></h2>
+                `<h2><span id="current-city">${currentWeather.name}</span>, <span id="current-date">${dayjs.unix(currentWeather.dt).format('MMMM D, YYYY')}</span> ${parseWeatherEmoji(currentWeather.weather[0].icon)} <img style="display:inline-block;" src="${iconURL}" /></h2>
             <h4>Temp: <span id="current-temp">${currentWeather.main.temp}</span></h4>
             <h4>Wind: <span id="current-wind">${currentWeather.wind.speed} mph</span></h4>
             <h4>Humidity: <span id="current-humidity">${currentWeather.main.humidity}</span></h4>`
@@ -35,15 +34,15 @@ function displayWeather(cityName) {
                 .then(function (fiveDayWeather) {
                     for (i = 2; i < fiveDayWeather.list.length; i = i + 8) {
                         console.log(fiveDayWeather.list[i])
-                        let fiveIconURL0 = `https://openweathermap.org/img/w/${fiveDayWeather.list[0].weather[0].icon}.png`;
-                        let fiveIconURL1 = `https://openweathermap.org/img/w/${fiveDayWeather.list[8].weather[0].icon}.png`;
-                        let fiveIconURL2 = `https://openweathermap.org/img/w/${fiveDayWeather.list[16].weather[0].icon}.png`;
-                        let fiveIconURL3 = `https://openweathermap.org/img/w/${fiveDayWeather.list[24].weather[0].icon}.png`;
-                        let fiveIconURL4 = `https://openweathermap.org/img/w/${fiveDayWeather.list[32].weather[0].icon}.png`;
+                        // let fiveIconURL0 = `https://openweathermap.org/img/w/${fiveDayWeather.list[0].weather[0].icon}.png`;
+                        // let fiveIconURL1 = `https://openweathermap.org/img/w/${fiveDayWeather.list[8].weather[0].icon}.png`;
+                        // let fiveIconURL2 = `https://openweathermap.org/img/w/${fiveDayWeather.list[16].weather[0].icon}.png`;
+                        // let fiveIconURL3 = `https://openweathermap.org/img/w/${fiveDayWeather.list[24].weather[0].icon}.png`;
+                        // let fiveIconURL4 = `https://openweathermap.org/img/w/${fiveDayWeather.list[32].weather[0].icon}.png`;
                         fiveDaySection.innerHTML =
-                        `<section class="five-day-child-container">
+                            `<section class="five-day-child-container">
                         <h3>${dayjs(fiveDayWeather.list[0].dt_txt).format('MMMM D, YYYY')}</h3>
-                        <p><img style="display:inline-block;" src="${fiveIconURL0}" /></p>
+                        <p>${parseWeatherEmoji(fiveDayWeather.list[0].weather[0].icon)}</p>
                         <p>Temp: ${fiveDayWeather.list[0].main.temp}</p>
                         <p>Wind: ${fiveDayWeather.list[0].wind.speed} mph</p>
                         <p>Humidity: ${fiveDayWeather.list[0].main.humidity}</p>            
@@ -81,12 +80,51 @@ function displayWeather(cityName) {
         })
 }
 
-let emoji = currentWeather.weather[0];
-if (currentWeather.weather[0] || fiveDayWeather.list[i].weather[0] === "01d" || "01n") {
-    emoji = "☀️";
+function parseWeatherEmoji(weatherCode) {
+    weatherCode = parseInt(weatherCode)
+    let emoji;
+    switch (weatherCode) {
+        case 1:
+            emoji = "☀️"
+            break;
+        case 2:
+            emoji = "🌤"
+            break;
+        case 3:
+            emoji = "☁️"
+            break;
+        case 4:
+            emoji = "☁️"
+            break;
+        case 9:
+            emoji = "🌧"
+            break;
+        case 10:
+            emoji = "🌧"
+            break;
+        case 11:
+            emoji = "⛈"
+            break;
+        case 13:
+            emoji = "❄️"
+            break;
+        case 50:
+            emoji = "💦"
+            break;
+        default:
+            break;
+    }
+    return emoji
 }
 
-// } else if (currentWeather.weather[0] || fiveDayWeather.list[i].weather[0] === "02d" || "02n") {
+// currentWeather.weather[0] can be {01d, 01n, 02d, 02n, etc.}
+
+// let emoji = currentWeather.weather[0];
+// if (currentWeather.weather[0] || fiveDayWeather.list[i].weather[0] === "01d" || "01n") {
+//     emoji = "☀️";
+// }
+
+// else if (currentWeather.weather[0] || fiveDayWeather.list[i].weather[0] === "02d" || "02n") {
 //     return "🌤";
 // } else if (currentWeather.weather[0] || fiveDayWeather.list[i].weather[0] === "03d" || "03n") {
 //     return "☁️";
